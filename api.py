28d7f2eb54cb94168b172d7ed98058c46892929e
mathfnz_macbook <matheus.fnds20@gmail.com>
2025-08-18 11:22:44 -0300

    add pandas on project

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 # %%
 
-import requests
-import json
+import requests # para realizar requisicoes na web
+import json # para tratar json de list/dict para arquivos
+from tqdm import tqdm
 
+# %% pegando os urls
 ceps: list = [
     "02423030",
     "01519000",
@@ -10,9 +12,11 @@ ceps: list = [
     "58038200"
 ]
 url = "https://viacep.com.br/ws/{cep}/json/"
-dados = []
- 
-for cep in ceps:   
+
+dados = [] # armazenamos os ceps em listas
+
+# navegamos nos ceps, verificamos se deu ok (200) e se sim, transformamos em json e adicionamos na lista
+for cep in tqdm(ceps):   
     resposta = requests.get(url.format(cep=cep))
     if resposta.status_code == 200:
         dados.append(resposta.json())
@@ -21,5 +25,6 @@ for cep in ceps:
 dados
 
 # %%
+# salvamos essas informacoes em um arquivo para 
 with open("ceps.json", "w", encoding="UTF-8") as open_file:
-    json.dump(dados, open_file)
+    json.dump(dados, open_file, ensure_ascii=False, indent=4)
